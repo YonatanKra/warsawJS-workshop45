e19fc66c6a0bb0ffcec354bcc243d658a1b77165
Yonatan <kra.yonatan@gmail.com>
2020-06-21 12:16:03 +0300

    apply test

---
+++ src/apply/apply.spec.ts
@@ -0,0 +1,13 @@
+import apply from './apply';
+
+describe('apply', () => {
+    it('should call the original function with arguments and return its result', () => {
+        const returnedValue = {};
+        const originalFunction = jest.fn().mockReturnValue(returnedValue);
+        const appliedArguments = [1,2,3];
+        const result = apply(originalFunction, {}, ...appliedArguments);
+
+        expect(originalFunction).toHaveBeenCalledWith(...appliedArguments);
+        expect(result).toEqual(returnedValue);
+    });
+});
